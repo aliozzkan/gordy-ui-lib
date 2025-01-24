54f1bc7c11920db3a8d6883a1124334119ef1df1
Ali Özkan <aliozzkan@gmail.com>
2025-01-24 10:13:23 +0300

    feat: eva-5166

--- src/components/ui/advanced-link.tsx
+++ src/components/ui/advanced-link.tsx
@@ -14,7 +14,10 @@ function AdvancedLink({ href, ...props }: AdvancedLinkProps) {
     if (checkInDataParam) {
       const checkInData = dayjs(checkInDataParam, "YYYY-MM-DD");
       if (checkInData.isValid() && checkInData.isBefore(dayjs())) {
-        urlParams.set("checkindate", dayjs().add(1, "week").format("YYYY-MM-DD"));
+        urlParams.set(
+          "checkindate",
+          dayjs().add(1, "week").format("YYYY-MM-DD")
+        );
         urlParams.set(
           "checkoutdate",
           dayjs().add(1, "week").add(1, "day").format("YYYY-MM-DD")
@@ -27,8 +30,14 @@ function AdvancedLink({ href, ...props }: AdvancedLinkProps) {
     if (departureDateParam) {
       const departureDate = dayjs(departureDateParam, "YYYY-MM-DD");
       if (departureDate.isValid() && departureDate.isBefore(dayjs())) {
-        urlParams.set("departureTime", dayjs().add(1, "week").format("YYYY-MM-DD"));
-        urlParams.set("returnTime", dayjs().add(1, "week").add(1, "day").format("YYYY-MM-DD"));
+        urlParams.set(
+          "departureTime",
+          dayjs().add(1, "week").format("YYYY-MM-DD")
+        );
+        urlParams.set(
+          "returnTime",
+          dayjs().add(1, "week").add(1, "day").format("YYYY-MM-DD")
+        );
       }
     }
 
@@ -37,7 +46,10 @@ function AdvancedLink({ href, ...props }: AdvancedLinkProps) {
     if (checkInDateParam2) {
       const checkInData = dayjs(checkInDateParam2, "YYYY-MM-DD");
       if (checkInData.isValid() && checkInData.isBefore(dayjs())) {
-        urlParams.set("checkInDate", dayjs().add(1, "week").format("YYYY-MM-DD"));
+        urlParams.set(
+          "checkInDate",
+          dayjs().add(1, "week").format("YYYY-MM-DD")
+        );
         urlParams.set(
           "checkOutDate",
           dayjs().add(1, "week").add(1, "day").format("YYYY-MM-DD")
@@ -50,15 +62,27 @@ function AdvancedLink({ href, ...props }: AdvancedLinkProps) {
     if (checkInDateParam3) {
       const checkInData = dayjs(checkInDateParam3, "YYYY-MM-DD");
       if (checkInData.isValid() && checkInData.isBefore(dayjs())) {
-        urlParams.set("checkinDate", dayjs().add(1, "week").format("YYYY-MM-DD"));
+        urlParams.set(
+          "checkinDate",
+          dayjs().add(1, "week").format("YYYY-MM-DD")
+        );
+
+        const isTourLink = href.includes("tour");
+
         urlParams.set(
           "checkoutDate",
-          dayjs().add(1, "week").add(1, "day").format("YYYY-MM-DD")
+          dayjs().add(1, isTourLink ? "year" : "week").format("YYYY-MM-DD")
         );
       }
     }
 
-    if (checkInDataParam || checkInDateParam2 || checkInDateParam3 || departureDateParam) return decodeURIComponent(urlParams.toString())
+    if (
+      checkInDataParam ||
+      checkInDateParam2 ||
+      checkInDateParam3 ||
+      departureDateParam
+    )
+      return decodeURIComponent(urlParams.toString());
     return href;
   }
 
